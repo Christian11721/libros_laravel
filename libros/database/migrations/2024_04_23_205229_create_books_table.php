16438185b3bd335a9c92531283d34a0553b6e22f
Christian11721 <christian_1321124087@uptecamac.edu.mx>
2024-04-24 11:49:00 -0600

    feat: migrations

---
+++ libros/database/migrations/2024_04_23_205229_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Author;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Author::class)->constrained();
+            $table->string('title');
+            $table->integer('publication_year');
+            $table->string('place_edition');
+            $table->string('editorial');
+            $table->integer('pages');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
